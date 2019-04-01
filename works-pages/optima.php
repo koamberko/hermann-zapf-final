450ec45c9b9528d1c00442ee6b8ca7ada89dcdf3
Amber Ko <koamberko@gmail.com>
2019-04-01 16:03:41 -0700

    Create optima.php

---
+++ works-pages/optima.php
@@ -0,0 +1,17 @@
+<?php include("header.php") ?>
+
+<center><img src="https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/115f1078304299.5ca15458614e7.png"></center>
+
+<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/51d53c78304299.5ca153b6c2adb.png" width="600"></center>
+  
+<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/bf139d78304299.5ca153b6c3550.png"></center>
+  
+<center>
+   <div class="backbutton">
+     <a href="http://hermannzapf.com//work.php">
+       Back to Work Overview
+     </a>
+	</div>
+</center>
+
+<?php include("footer.php") ?>
