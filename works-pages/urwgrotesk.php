c592140452daa2582b4f2cb82d57ee0d90640d7b
Amber Ko <koamberko@gmail.com>
2019-04-01 16:03:44 -0700

    Create urwgrotesk.php

---
+++ works-pages/urwgrotesk.php
@@ -0,0 +1,17 @@
+<?php include("header.php") ?>
+
+<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/ea355878309033.5ca18b4aa02a4.png"></center>
+
+<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5c8b0b78309033.5ca18b4aa05c1.png" width="600"></center>
+  
+<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0de83378309033.5ca18b4a9ff53.png"></center>
+  
+<center>
+   <div class="backbutton">
+     <a href="http://hermannzapf.com//work.php">
+       Back to Work Overview
+     </a>
+	</div>
+</center>
+
+<?php include("footer.php") ?>
