bdabe1b13c0953c0f05b2de287b606113b3b055b
Amber Ko <koamberko@gmail.com>
2019-04-01 16:03:29 -0700

    Create index.php

---
+++ Nav/index.php
@@ -0,0 +1,44 @@
+<?php include("header.php") ?>
+
+<img class="gif" src="https://mir-cdn.behance.net/v1/rendition/project_modules/fs/634ef077640815.5c903aac44ae8.gif">
+
+<div class="notable">notable works</div>
+<div class="notable2">notable works</div>
+<div class="see-all"> <a href="<?php echo '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>/work.php">see all</div>
+
+<div class="container"><img src="https://i.pinimg.com/564x/76/2f/b4/762fb4675291ec999438df8e5c15ee69.jpg" alt=""/>
+  <div class="inner">
+    <div class="v-inner">
+      <h3>Typeface | Optima</h3>
+      <p>A typeface that combines features of both serif and sans serif types into one humanistic design.</p>
+    </div>
+  </div>
+</div>
+<div class="container"><img src="https://i.pinimg.com/564x/92/88/81/92888105973627b05ba36c7947815c72.jpg" alt=""/>
+  <div class="inner">
+    <div class="v-inner">
+      <h3>Typeface | Palatino</h3>
+      <p>One of the top ten most-used typefaces in the world, it is a classical serif typeface that employs a strong, open style that’s highly legible. Very attractive and easily readable, its popularity has spawned a host of imitators.</p>
+    </div>
+  </div>
+</div>
+<div class="container"><img src="https://i.pinimg.com/564x/45/69/d9/4569d946fa8cb20e3d5c36bd6dc23064.jpg" alt=""/>
+  <div class="inner">
+    <div class="v-inner">
+      <h3>Typeface | URW Grotesk</h3>
+      <p>A simple sans serif typeface that's esigned specifically for use in books, newspapers, and magazines.</p>
+    </div>
+  </div>
+</div>
+<div class="container"><img .comenius=".comenius" src="https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/da000977829351.5c92e2659c951.png" alt=""/>
+  <div class="inner">
+    <div class="v-inner">
+      <h3>Typeface | Comenius</h3>
+      <p>Mainly designed for large bodies of text, it can also be used for ads and display purposes, especially in its bold weight.</p>
+    </div>
+  </div>
+</div>
+
+
+<?php include("footer.php") ?>
+
