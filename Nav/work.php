d5574d3d488ff77dca94a32fdd362362e5cb5a8d
Amber Ko <koamberko@gmail.com>
2019-04-01 16:03:37 -0700

    Create work.php

---
+++ Nav/work.php
@@ -0,0 +1,47 @@
+<?php include("header.php") ?>
+
+<html>
+  
+<body>
+  <div class="filter">Filter</div>
+  
+  <div class="optimas">
+    <img src="https://mir-cdn.behance.net/v1/rendition/project_modules/max_1200/2e381f78300913.5ca13056e4551.png" width="500">
+  </div>
+  <div class="palatinos">
+    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/cdec9078300913.5ca13056e4985.png" width="500">
+  </div>
+  <div class="zapfinos">
+    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/7d602b78300913.5ca13056e52bd.png" width="500">
+  </div>
+  <div class="urwgrotesks">
+    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0fd51e78300913.5ca13056e4d4b.png" width="500">
+  </div>
+  <div class="zapfbooks">
+    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/391a4b78300913.5ca13056e4f4c.png" width="500">
+  </div>
+  <div class="michelangelos">
+    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d990ce78300913.5ca13056e56a5.png" width="500">
+  </div>
+  
+  <div class="optimag">
+    <a href="optima.php"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5be4ec78300973.5ca130bca6fbd.gif" width="500"></a>
+  </div>
+  <div class="palatinog">
+    <a href="palatino.php"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e8d64578300973.5ca130bca7629.gif" width="500"></a>
+  </div>
+  <div class="zapfinog">
+    <a href="zapfino.php"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/bb495378300973.5ca130bca73e5.gif" width="500"></a>
+  </div>
+  <div class="urwgroteskg">
+    <a href="urwgrotesk.php"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6315678300973.5ca130bca6925.gif" width="500"></a>
+  </div>
+  <div class="zapfbookg">
+    <a href="zapfbook.php"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2a3f9878300973.5ca130bca665b.gif" width="500"></a>
+  </div>
+  <div class="michelangelog">
+    <a href="michelangelo.php"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e2a86078300973.5ca130bca7c9c.gif" width="500"></a>
+  </div>
+
+<?php include("footer.php") ?>
+
