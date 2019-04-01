cb2b330d90103dc80af7cf7bd460b740a185a872
Amber Ko <koamberko@gmail.com>
2019-04-01 16:03:42 -0700

    Create palatino.php

---
+++ works-pages/palatino.php
@@ -0,0 +1,17 @@
+<?php include("header.php") ?>
+
+<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/68c43078308437.5ca1855b9e222.png"></center>
+
+<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/153dc078308437.5ca1855b9dcc7.png" width="600"></center>
+  
+<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/55957078308437.5ca1855b9e01c.png"></center>
+  
+<center>
+   <div class="backbutton">
+     <a href="http://hermannzapf.com//work.php">
+       Back to Work Overview
+     </a>
+	</div>
+</center>
+
+<?php include("footer.php") ?>
