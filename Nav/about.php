dd6d9c72bda4f708634c3e87ed0e35992f725f63
Amber Ko <koamberko@gmail.com>
2019-04-01 16:06:25 -0700

    Update about.php

--- Nav/about.php
+++ Nav/about.php
@@ -4,15 +4,16 @@
 	<div class="atd">
 		<h1>About The <br> Designer</h1>
 	</div>
-  <div class="atdo">
+  	<div class="atdo">
     <h1>About The <br> Designer</h1>
-  </div>
-  <div class="vlatd"></div>
-   <p class="bio">Hermann Zapf was a typeface designer and calligrapher who was born in November of 1918 in Nuremburg, Germany. Zapf designed 50 typefaces in his lifetime, his most well known and popular ones being <em>Optima</em>, <em>Palatino</em>, and <em>Zapfino</em>. During his designing career, he covered 3 stages of printing technology; hot metal composition, phototypesetting, and digital typography. He specialized working with calligraphy, book typography, and wrote a lot on graphic design. Initially, Zapf was interested in technical subjects, and wanted to pursue a career in electrical engineering. Due to changes in the political regime at the time, he couldn’t follow his initial plan to go to school for those interests. Instead, he was 
-recommended to pursue lithography, and was introduced to calligraphy. During the World War, he worked desk jobs because of health problems caused by hard labour while conscripted as a soldier. Eventually, Zapf took up typography and computer typography. Later on in his life, he moved to the US, where he had more success than in Germany as his work was taken more seriously. He was married to Gudrun Zapf von Hesse, who was also a calligrapher and typeface designer. He died at the age of 96 years recently in 2015 in Germany. <br> This website's visual inspiration was taken from Zapf's graphic style in the creation of his dingbats, as pictured below.
+  	</div>
+ 	<div class="vlatd"></div>
+   		<p class="bio">Hermann Zapf was a typeface designer and calligrapher who was born in November of 1918 in Nuremburg, Germany. Zapf designed 50 typefaces in his lifetime, his most well known and popular ones being <em>Optima</em>, <em>Palatino</em>, and <em>Zapfino</em>. During his designing career, he covered 3 stages of printing technology; hot metal composition, phototypesetting, and digital typography. He specialized working with calligraphy, book typography, and wrote a lot on graphic design. Initially, Zapf was interested in technical subjects, and wanted to pursue a career in electrical engineering. Due to changes in the political regime at the time, he couldn’t follow his initial plan to go to school for those interests. Instead, he was recommended to pursue lithography, and was introduced to calligraphy. During the World War, he worked desk jobs because of health problems caused by hard labour while conscripted as a soldier. Eventually, Zapf took up typography and computer typography. Later on in his life, he moved to the US, where he had more success than in Germany as his work was taken more seriously. He was married to Gudrun Zapf von Hesse, who was also a calligrapher and typeface designer. He died at the age of 96 years recently in 2015 in Germany. <br> This website's visual inspiration was taken from Zapf's graphic style in the creation of his dingbats, as pictured below.</p>
  </body>
 
-<center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/af9ad778305455.5ca1623bca673.png" width="1000"></center>
+<center>
+	<img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/af9ad778305455.5ca1623bca673.png" width="1000">
+</center>
 
 <?php include("bluefooter.php") ?>
 
