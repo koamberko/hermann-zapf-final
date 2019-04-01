f1257afd1c787c09b5651c2a9fa16fbb82e327f6
Amber Ko <koamberko@gmail.com>
2019-04-01 16:03:35 -0700

    Create skillset.php

---
+++ Nav/skillset.php
@@ -0,0 +1,37 @@
+<?php include("header.php") ?>
+
+<body>
+    <div class="container">
+        <div class="hmc">
+            <h1>Hot Metal Composition</h1>
+        </div>
+        <div class="hmco">
+            <h1>Hot Metal Composition</h1>
+        </div>
+        <div class="hmct">
+            <p>In printing and typography, hot metal typesetting (also called mechanical typesetting, hot lead typesetting, hot metal, and hot type) is a technology for typesetting text in letterpress printing. This method injects molten type metal into
+                a mold that has the shape of one or more glyphs. The resulting sorts or slugs are later used to press ink onto paper. Normally the typecasting machine would be controlled by a keyboard or by a paper tape.
+                <br> Hot metal typesetting was developed in the late nineteenth century as a development of conventional cast metal type.[1] The technology had several advantages: it reduced labour since type sorts did not need to be slotted into position
+                manually, and cast crisp new type for each printing job. In the case of Linotype machines, each line was cast as a robust continuous block (hence "line o'type") which was useful for rapid newspaper printing. It was the standard technology
+                used for mass-market printing from the late nineteenth century, finally declining with the arrival of phototypesetting and then electronic processes in the 1950s to 1980s.</p>
+        </div>
+        <img class="image1" src="https://mir-cdn.behance.net/v1/rendition/project_modules/fs/41c3c378153515.5c9c5d262328b.png" width="1450" height="800">
+        <div class="vl"></div>
+        <div class="pts">
+            <h1>Phototypesetting</h1>
+        </div>
+        <div class="ptso">
+            <h1>Phototypesetting</h1>
+        </div>
+        <div class="ptst">
+            <p>Phototypesetting is a method of setting type, rendered obsolete with the popularity of the personal computer and desktop publishing software, that uses a photographic process to generate columns of type on a scroll of photographic paper. <br>                The first phototypesetters quickly project light through a film negative image of an individual character in a font, then through a lens that magnifies or reduces the size of the character onto photographic paper, which is collected on
+                a spool in a light-proof canister. The photographic paper or film is then fed into a processor—a machine that pulls the paper or film strip through two or three baths of chemicals—where it emerges ready for paste-up or film make-up. Later
+                phototypesetting machines used alternative methods, such as displaying a digitised character on a CRT screen.
+                <br> Phototypesetting offered numerous advantages over metal type, including the lack of need to keep heavy metal type and matrices in stock, the ability to use a much wider range of fonts and graphics and print them at any desired size,
+                as well as faster page layout setting.</p>
+        </div>
+        <img class="image2" src="https://mir-cdn.behance.net/v1/rendition/project_modules/fs/89f8d278153515.5c9c5d2623580.png" width="1450" height="800">
+        <center><img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/9f9d6378153515.5ca194b0178c6.png" width="11" style="position:relative;left:-153px;top:-1700px"></center>
+    </div>
+</body>
+<?php include("bluefooter.php") ?>
